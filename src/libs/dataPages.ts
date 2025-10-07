5eb71231e6eed36ef32a33c9d96c567a1d9e9e63
jorge-devtai <jorge.deleon@taiarts.com>
2025-10-07 10:09:26 +0200

    Refactor BaseLayout and data fetching logic; implement getAllPagesBySlug function and update index page to utilize dynamic slugs

--- src/libs/dataPages.ts
+++ src/libs/dataPages.ts
@@ -10,7 +10,7 @@ export async function getDataPages() {
         
         console.log("Raw Data Pages:", data);
 
-        const [dataPage] = data.data.map((page: any) => {
+        const dataPage = data.data.map((page: any) => {
         const { 
             id, 
             attributes: { 
@@ -32,10 +32,30 @@ export async function getDataPages() {
 
         console.log("Data Pages fetched:", dataPage);
 
-        return data;   
+        return dataPage;   
 
     } catch (error) {
         console.error("Error fetching pages:", error);
         return null;
     }
 }
+
+export async function getAllPagesBySlug() {
+    try{
+        const response = await fetch(`${endpoints.pages}?fields[node--page]=path`);
+        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+
+        const data = await response.json();
+        if (!Array.isArray(data.data) || data.length === 0) throw new Error("Data is not in expected format or is empty");
+
+        const slugPages = data.data.map((page: any ) => page.attributes?.path?.alias || "");
+
+        console.log("Pages fetched by slug:", slugPages);
+
+        return slugPages;
+        
+    } catch (error) {
+        console.error("Error fetching page by slug:", error);
+        return null;
+    }
+}   
